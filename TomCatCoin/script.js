3ea2edd6ec92d1037566620c1e4f61543ee4e360
barzoid1 <128053888+barzoid1@users.noreply.github.com>
2023-04-16 12:14:56 +0100

    Add files via upload

---
+++ TomCatCoin/script.js
@@ -0,0 +1,34 @@
+const text = document.querySelector('#text').innerText
+const copybtn = document.getElementById('copy')
+async function copyToClip (){
+   try {
+    await  navigator.clipboard.writeText(text)
+   } catch (error) {
+    console.log('Copy action failed')
+   }
+}
+
+const copy = () => { 
+  copyToClip()
+  copybtn.innerText = 'Copied'
+  setTimeout(() => {
+    copybtn.innerText = 'Copy'
+  }, 1000); 
+}
+
+const load = () => {
+  const loader = document.querySelector('#loader')
+  setTimeout(() => {
+loader.style.display = 'none'
+loader.parentElement.removeChild(loader)
+  }, 2000);
+  loader.style.className.add('loader')   
+ 
+  
+
+}
+ load()
+
+ let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
